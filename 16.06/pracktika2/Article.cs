4b6a673741768de50233ba14c07df1b4bdcff8fe
ITroianPowerI <107670729+ITroianPowerI@users.noreply.github.com>
2022-06-23 09:47:40 +0300

    Add files via upload

---
+++ 16.06/pracktika2/Article.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracktika2
+{
+    class Article
+    {
+        private string name;
+        private string shop;
+        private double price;
+        public Article(string Name, string Shop, double Price)
+        {
+            name = Name;
+            shop = Shop;
+            price = Price;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Shop
+        {
+            get
+            {
+                return shop;
+            }
+            set
+            {
+                shop = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
