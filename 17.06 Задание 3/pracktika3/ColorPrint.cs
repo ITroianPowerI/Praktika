9ef8560f659edc2db037453d34afe4047c3d0978
ITroianPowerI <107670729+ITroianPowerI@users.noreply.github.com>
2022-06-24 13:52:36 +0300

    Add files via upload

---
+++ 17.06 Задание 3/pracktika3/ColorPrint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracktika3
+{
+    static class ColorPrint
+    {
+
+        public static void Print(string stroka, int color)
+        {
+            switch (color)
+            {
+                case (int)Color.Red:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    }
+                case (int)Color.Blue:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    }
+                case (int)Color.Green:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    }
+                case (int)Color.Yellow:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Введенная Вами строка будет выведена на экран цветом по умолчанию!");
+                    break;
+            }
+
+            Console.WriteLine(stroka);
+        }
+    }
+    enum Color
+    {
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+        Yellow = 4
+    }
+}
---
+++ 17.06 Задание 3/pracktika3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracktika3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Укажите цвет: (1 - красный, 2 - синий,  3 - зелёный, 4 - жёлтый)");
+            int color = Convert.ToInt32(Console.ReadLine());
+            ColorPrint.Print(stroka, color);
+            Console.ReadKey();
+        }
+    }
+}
